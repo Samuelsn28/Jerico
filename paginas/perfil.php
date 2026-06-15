d3440e702d86d86c3046d2fd4bc583751f9a00ec
Samuel <157188802+Samuelsn28@users.noreply.github.com>
2026-06-15 13:46:45 -0300

    Mudanças

---
+++ paginas/perfil.php
@@ -0,0 +1,28 @@
+<?php 
+	session_set_cookie_params(["httponly" => true]);
+	session_start();
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<title>Perfil - Jerico</title>
+		<link rel="stylesheet" href="../padrao.css">
+	</head>
+	<body>
+		<section>
+			<h2>
+				Noname
+			</h2>
+			<section>
+				<p>E-mail:</p>
+				<p>email@email.com</p>
+			</section>
+		</section>
+	</body>
+</html>
+<?php 
+	
+	
+
+?>
